97d78152c8bd5395619099ca37cdbce4a3c74c39
Masaki Ohno <masakio@post.kek.jp>
2018-06-14 14:15:08 +0900

    テストで関数追加

--- hello.py
+++ hello.py
@@ -5,8 +5,18 @@ def to_doubles(number: int) -> int:
     """
     return number * 2
 
+def add_owata(text: str) -> str:
+    """
+    入力された文字列に＼(^o^)／ｵﾜﾀを加える
+    """
+    return text + '＼(^o^)／ｵﾜﾀ'
+
 if __name__ == '__main__':
 
     print('hello world')
+
     ans = to_doubles(23)
     print(ans)
+
+    owata = add_owata('富士通の小学生向けPC')
+    print(owata)
